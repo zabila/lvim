e85cc9e089254e48106df4df4f7ab10041d2ec3c
Volodymyr Zabila <wzabila1995@gmail.com>
2023-07-23 13:48:46 +0300

    Added themes tokionight

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -47,8 +47,76 @@ gruvbox.setup({
     transparent_mode = true,
 })
 
+local status_tokionight_ok, tokionight = pcall(require, "tokionight")
+if not status_tokionight_ok then
+    print("Tokionight don't installed")
+end
+
+tokionight.setup({
+    style = "storm",
+    light_style = "night",
+    transparent = false,
+    terminal_colors = true,
+    styles = {
+        comments = { italic = true },
+        keywords = { italic = true },
+        strings = { italic = true },
+        functions = {},
+        variables = {},
+        sidebars = "dark",
+        floats = "dark",
+    },
+    sidebars = { "qf", "vista_kind", "terminal", "packer" },
+    day_brightness = 0.3,
+    hide_inactive_statusline = false,
+    dim_inactive = false,
+    lualine_bold = false,
+
+    on_colors = function(colors)
+        colors.hint = colors.orange
+        colors.error = "#ff0000"
+    end,
+
+    on_highlights = function(hl, c)
+        local prompt = "#2d3149"
+        hl.TelescopeNormal = {
+            bg = c.bg_dark,
+            fg = c.fg_dark,
+        }
+        hl.TelescopeBorder = {
+            bg = c.bg_dark,
+            fg = c.bg_dark,
+        }
+        hl.TelescopePromptNormal = {
+            bg = prompt,
+        }
+        hl.TelescopePromptBorder = {
+            bg = prompt,
+            fg = prompt,
+        }
+        hl.TelescopePromptTitle = {
+            bg = prompt,
+            fg = prompt,
+        }
+        hl.TelescopePreviewTitle = {
+            bg = c.bg_dark,
+            fg = c.bg_dark,
+        }
+        hl.TelescopeResultsTitle = {
+            bg = c.bg_dark,
+            fg = c.bg_dark,
+        }
+    end,
+})
+
+lvim.builtin.lualine.on_config_done = {
+    options = {
+        theme = 'tokionight'
+    }
+}
+
 lvim.transparent_window = false
-lvim.colorscheme = themes.tokyonight_night
+lvim.colorscheme = themes.tokyonight
 
 local extra_opts = {
     sonokai = {
